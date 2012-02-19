0c59ea65b26c5e9cedcfb2c84e23c43204313ed8
Tianyang Li <tmy1018@gmail.com>
2012-02-19 16:46:41 +0800

    do things faster

---
+++ zhongart/zhongart.js
@@ -0,0 +1,11 @@
+/*
+ *  zhongart.js
+ *
+ *  Create Chinese character ASCII art 
+ *  from an image using HTML5 and Javascript
+ *
+ *  Released under GPLv3
+ *  
+ *  Copyright (C) 2012 Tianyang Li
+ */
+
